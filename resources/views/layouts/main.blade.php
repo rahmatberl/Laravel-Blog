ecc2578daffcd8573213834d6c6ec40db9195057
rahmatberl <rahmatberlianda@gmail.com>
2021-12-27 16:11:06 +0700

    Navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,21 +11,7 @@
     <title>Rahmat Blog</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div class="container">
-        <a class="navbar-brand" href="#">Rahmat Blog</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active"href="/">Home</a>
-            <a class="nav-link" href="/about">About</a>
-            <a class="nav-link" href="/post">Blog</a>
-          </div>
-        </div>
-      </div>
-    </nav>
+    @include('components.navbar')
     <div  class="container mt-4">
         @yield('container')
     </div>
