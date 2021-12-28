947e4874c43c4d3217aea3c8c6571cd5a903c1ba
rahmatberl <rahmatberlianda@gmail.com>
2021-12-28 10:33:45 +0700

    post

--- routes/web.php
+++ routes/web.php
@@ -51,5 +51,32 @@ Route::get('/posts', function () {
 
 ///Halaman single post
 Route::get('posts/{slug}', function ($slug) {
-    return view('post');  
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Rahmat Berlianda Avandi",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis vitae dolores perspiciatis quos, numquam aspernatur illum, odio officia veritatis repudiandae totam velit sequi praesentium itaque voluptate, adipisci dignissimos tempora? Eveniet minus nemo numquam quas dolorem consectetur deserunt totam itaque ea ut facilis quisquam assumenda doloremque laboriosam fugiat doloribus maxime nisi voluptatibus impedit quibusdam, tempora quos! Ab, sapiente, minima veritatis provident quod iusto, voluptate quam vero sequi impedit beatae? Exercitationem necessitatibus magni laudantium consequuntur nisi labore natus nihil, maxime enim. Deserunt.",
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Robert Caligari",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus harum quaerat consequuntur debitis incidunt vitae architecto soluta unde, tempore at delectus, numquam doloribus corporis non saepe sit fugit voluptatibus veritatis praesentium! Veritatis, cum enim molestiae eligendi, fugiat velit dolor possimus consectetur labore facere, minus illum laudantium. Rem fugit inventore temporibus expedita saepe, hic odit sunt ullam enim repellat. Hic vel, ullam perferendis vitae et pariatur. Rem velit tempora reiciendis neque. In, enim? Natus recusandae iste harum, quasi nemo, deserunt atque sapiente impedit dicta ut minus! Tempore facilis cumque enim ullam cupiditate, quo earum corrupti iste ad, non eveniet ab repudiandae!",
+        ],
+    ];
+
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);  
 });
